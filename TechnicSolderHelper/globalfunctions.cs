95039d6f72def4159aacc05514c96c68ed6e6bfe
rasmus <hansen13579@gmail.com>
2014-10-11 21:20:05 +0200

    Remember all the files!!

---
+++ TechnicSolderHelper/globalfunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TechnicSolderHelper
+{
+	public class globalfunctions
+	{
+		public globalfunctions ()
+		{
+		}
+
+		public static Boolean isUnix() {
+			return Environment.OSVersion.ToString ().ToLower ().Contains ("unix");
+		}
+	}
+}
+
