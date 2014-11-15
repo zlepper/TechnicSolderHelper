49e68c7e19521a2681b6eebe890b8ce8024789eb
Rasmus Hansen <hansen13579@gmail.com>
2014-11-15 16:36:44 +0100

    Add missing file

---
+++ TechnicSolderHelper/modpacks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechnicSolderHelper
+{
+    public class modpacks
+    {
+        public Dictionary<String, List<String>> modpack;
+    }
+}
+
