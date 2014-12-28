ac4da5ac9350c05c5dde8b7319a332edf47d2f69
Rasmus Hansen <hansen13579@gmail.com>
2014-12-28 10:10:48 +0100

    Remember all the files

---
+++ TechnicSolderHelper/FileUpload/ftp.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security;
+using TechnicSolderHelper.confighandler;
+using TechnicSolderHelper.cryptography;
+
+namespace TechnicSolderHelper.FileUpload
+{
+    public class Ftp
+    {
+        private String userName;
+        private String password;
+        private String url;
+
+        public Ftp()
+        {
+            Crypto crypto = new Crypto();
+            if (globalfunctions.isUnix())
+            {
+                ConfigHandler ch = new ConfigHandler();
+                url = ch.getConfig("ftpUrl");
+                try
+                {
+                    password = crypto.DecryptString(ch.getConfig("ftpPassword"));
+                }
+                catch (System.Exception)
+                {
+                    password = "";
+                }
+                userName = ch.getConfig("ftpUserName");
+            }
+            else
+            {
+                url = Properties.Settings.Default.ftpUrl;
+                userName = Properties.Settings.Default.ftpUserName;
+                try
+                {
+                    password = crypto.DecryptString(Properties.Settings.Default.ftpPassword);
+                }
+                catch (Exception)
+                {
+                    Properties.Settings.Default.ftpPassword = crypto.EncryptToString("password");
+                    password = "password";
+                }
+            }
+            if (url.EndsWith("/"))
+            {
+                url.Remove(url.Length - 1);
+            }
+            if (!url.StartsWith("ftp://"))
+            {
+                url = "ftp://" + url;
+            }
+        }
+
+        public void uploadFolder(String folderPath)
+        {
+            String[] files;
+            try
+            {
+                files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                Debug.WriteLine("No files was created, so no reason to upload anything");
+                return;
+            }
+
+            foreach (String file in files)
+            {
+                Debug.WriteLine("");
+                Debug.WriteLine(file);
+                Debug.WriteLine(file.Replace(folderPath + globalfunctions.pathSeperator, ""));
+                uploadFile(file, file.Replace(folderPath + globalfunctions.pathSeperator, ""), "mods");
+            }
+
+        }
+
+        private List<String> getDirectoryContent(String location)
+        {
+            List<String> folderContent = new List<string>();
+            FtpWebRequest request;
+            if (location == null)
+            {
+                request = (FtpWebRequest)WebRequest.Create(this.url);
+            }
+            else
+            {
+                request = (FtpWebRequest)WebRequest.Create(location);
+            }
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            request.Credentials = new NetworkCredential(userName, password);
+
+            using (FtpWebResponse responce = (FtpWebResponse)request.GetResponse())
+            {
+                Stream responceStream = responce.GetResponseStream();
+                using (StreamReader reader = new StreamReader(responceStream))
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            String s = reader.ReadLine();
+                            if (s.Contains("/"))
+                            {
+                                s = s.Substring(s.LastIndexOf("/") + 1);
+                            }
+                            folderContent.Add(s);
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return folderContent;
+        }
+
+        public void uploadFile(String FullyQualifiedPathName, String destinationOnServer, String constant)
+        {
+            getDirectoryContent(null);
+            if (constant != null)
+            {
+                destinationOnServer = constant + globalfunctions.pathSeperator + destinationOnServer;
+            }
+            String[] tmp = destinationOnServer.Split(globalfunctions.pathSeperator);
+            List<String> folders = new List<String>(tmp);
+            String fileToUpload = folders[folders.Count - 1];
+            folders.Remove(folders[folders.Count - 1]);
+            for (int i = 0; i < folders.Count; i++)
+            {
+                folders[i] = folders[i].Replace(globalfunctions.pathSeperator.ToString(), "");
+            }
+            FtpWebRequest request = WebRequest.Create(url) as FtpWebRequest;
+            request.Credentials = new NetworkCredential(this.userName, this.password);
+            Debug.WriteLine(request.RequestUri);
+            foreach (String folder in folders)
+            {
+                Debug.WriteLine(folder);
+                List<String> directoryContent = getDirectoryContent(request.RequestUri.ToString());
+                if (directoryContent.Contains(folder))
+                {
+                    if (request.RequestUri.ToString().EndsWith("/"))
+                    {
+                        request = WebRequest.Create(request.RequestUri + folder) as FtpWebRequest;
+                    }
+                    else
+                    {
+                        request = WebRequest.Create(request.RequestUri + "/" + folder) as FtpWebRequest;
+                    }
+                    Debug.WriteLine(request.RequestUri);
+                }
+                else
+                {
+                    if (request.RequestUri.ToString().EndsWith("/"))
+                    {
+                        request = WebRequest.Create(request.RequestUri + folder) as FtpWebRequest;
+                    }
+                    else
+                    {
+                        request = WebRequest.Create(request.RequestUri + "/" + folder) as FtpWebRequest;
+                    }
+                    Debug.WriteLine(request.RequestUri);
+                    request.Credentials = new NetworkCredential(this.userName, this.password);
+                    request.Method = WebRequestMethods.Ftp.MakeDirectory;
+                    try
+                    {
+                        FtpWebResponse responce = (FtpWebResponse)request.GetResponse();
+                        Debug.WriteLine(responce.StatusDescription);
+                    }
+                    catch (System.Net.WebException e)
+                    {
+                        Debug.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            try
+            {
+                Debug.WriteLine("Uploading file: " + fileToUpload);
+                request = WebRequest.Create(request.RequestUri + "/" + fileToUpload) as FtpWebRequest;
+                request.Credentials = new NetworkCredential(this.userName, this.password);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.UseBinary = true;
+                request.UsePassive = true;
+                request.KeepAlive = true;
+                using (FileStream fs = File.OpenRead(FullyQualifiedPathName))
+                {
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, buffer.Length);
+                    fs.Close();
+                    Stream requestStream = request.GetRequestStream();
+                    requestStream.Write(buffer, 0, buffer.Length);
+                    requestStream.Close();
+                    requestStream.Flush();
+                }
+            }
+            catch (System.Net.WebException e)
+            {
+                //Console.WriteLine("error getting responce");
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
+
---
+++ TechnicSolderHelper/FileUpload/ftpInfo.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TechnicSolderHelper.FileUpload
+{
+    partial class ftpInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Host = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Acceptbutton = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(12, 27);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(179, 20);
+            this.Username.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(12, 74);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(179, 20);
+            this.Password.TabIndex = 1;
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // Host
+            // 
+            this.Host.Location = new System.Drawing.Point(12, 120);
+            this.Host.Name = "Host";
+            this.Host.Size = new System.Drawing.Size(179, 20);
+            this.Host.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Host";
+            // 
+            // Acceptbutton
+            // 
+            this.Acceptbutton.Location = new System.Drawing.Point(12, 146);
+            this.Acceptbutton.Name = "Acceptbutton";
+            this.Acceptbutton.Size = new System.Drawing.Size(52, 26);
+            this.Acceptbutton.TabIndex = 3;
+            this.Acceptbutton.Text = "Accept";
+            this.Acceptbutton.UseVisualStyleBackColor = true;
+            this.Acceptbutton.Click += new System.EventHandler(this.Acceptbutton_Click);
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(70, 146);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(55, 26);
+            this.test.TabIndex = 4;
+            this.test.Text = "test";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ftpInfo
+            // 
+            this.AcceptButton = this.Acceptbutton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 184);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.Acceptbutton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Host);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ftpInfo";
+            this.Text = "ftpInfo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Host;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Acceptbutton;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.Button button1;
+    }
+}
